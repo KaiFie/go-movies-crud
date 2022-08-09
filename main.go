3edadbeb9e3586c7537c92c8b337e6d4ef12f702
Илья Мудрицкий <askold@Ilas-MacBook-Air.local>
2022-08-09 16:47:38 +0300

    Start of project

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"encoding/json"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID string `json:"id"`
+	Isbn string `json:"isbn"`
+	Title string `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{ID: "1", Isbn: "236915", Title: "Межсезонье", Director: &Director{Firstname: "Евгений", Lastname: "Сладкоедов"}})
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("PUT")
+
+	fmt.Println("Starting server at port 8000")
+	log.Fatal(http.ListenAndServe(":8000", r))
+
+}
